dbd9a503f97a4a34f5dcfc759b28c2866fdab4ec
Simon-Bertram <simonbertram@hotmail.com>
2024-06-19 21:20:36 +0100

    Clean up app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <h1>Recipies for Ninjas</h1>
+          {children}
+        </nav>
+      </body>
     </html>
   );
 }
